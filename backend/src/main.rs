487a784398ac600c41811597523543fdad45094b
Sadagat Asgarov <sadagat.asgarov@gmail.com>
2025-01-18 12:11:25 +0400

    backend yazmaga baslaqiq ammma databazede bira xetalar var helelik

--- backend/src/main.rs
+++ backend/src/main.rs
@@ -1,19 +1,31 @@
 use std::collections::HashMap;
-
+use serde::{Serialize, Deserialize};
 use moon::*;
 use sqlx::{query, Executor, PgPool};
+
 
 async fn frontend() -> Frontend {
     Frontend::new().title("New Project")
 }
 
-async fn up_msg_handler(_: UpMsgRequest<()>) {}
+async fn up_msg_handler(req: UpMsgRequest<()>) {
+        let UpMsgRequest {
+            up_msg,
+            ..
+        } = req;
 
-struct Author {
-    _id: i32,
-    name: String,
-    country: String,
+        #[derive(Debug, Serialize, Deserialize, sqlx::FromRow)]
+        #[serde(crate = "serde")]
+        struct U{}
+
+        let _down_msg = match up_msg {
+            // ------- Auth --------
+            UpMsg::Login {
+            
+            }
+        };
 }
+
 
 #[moon::main]
 async fn main() -> std::io::Result<()> {
@@ -23,65 +35,6 @@ async fn main() -> std::io::Result<()> {
         .expect("Env var DATABASE_URL is required for this example.");
     let pool = PgPool::connect(&conn_str).await.expect("Failed to connect to the database");
 
-    // Veritabanı tablolarını oluştur
-    pool.execute(
-        "
-        CREATE TABLE IF NOT EXISTS author (
-            id      SERIAL PRIMARY KEY,
-            name    VARCHAR NOT NULL,
-            country VARCHAR NOT NULL
-        );
-    ",
-    )
-    .await
-    .expect("Failed to create author table");
-
-    pool.execute(
-        "
-        CREATE TABLE IF NOT EXISTS book (
-            id        SERIAL PRIMARY KEY,
-            title     VARCHAR NOT NULL,
-            author_id INTEGER NOT NULL REFERENCES author
-        );
-    ",
-    )
-    .await
-    .expect("Failed to create book table");
-
-    pool.execute(
-        "
-        CREATE TABLE IF NOT EXISTS defter (
-            id        SERIAL PRIMARY KEY,
-            title     VARCHAR NOT NULL,
-            author_id INTEGER NOT NULL REFERENCES author
-        );
-    ",
-    )
-    .await
-    .expect("Failed to create defter table");
-
-    // Yazarları ekleme
-    let mut authors = HashMap::new();
-    authors.insert(String::from("Sadagat Asgarov"), "Azerbaijan");
-    authors.insert("Sakhavat Asgarov".to_string(), "Nakhchivan");
-
-    for (name, country) in &authors {
-        let author = Author {
-            _id: 0,
-            name: name.to_string(),
-            country: country.to_string(),
-        };
-
-        // SQL sorgusunu parametrelerle çalıştır
-        query!(
-            "INSERT INTO author (name, country) VALUES ($1, $2)",
-            author.name,
-            author.country
-        )
-        .execute(&pool)
-        .await
-        .expect("Failed to insert author");
-    }
 
     // Moon uygulamasını başlat
     start(frontend, up_msg_handler, |_| {}).await
--- shared/src/lib.rs
+++ shared/src/lib.rs
@@ -1,1 +1,75 @@
+// use models::{
+//     school::FullSchool,
+//     users::ResetForm,
+// };
+use moonlight::*;
 
+// use msgs::{
+//     classes::{ClassDownMsgs, ClassUpMsgs},
+//     teachers::{TeacherDownMsgs, TeacherUpMsgs}, lectures::{LecturesUpMsg, LecturesDownMsg}, timetables::{TimetablesUpMsgs, TimetablesDownMsgs}, admin::{AdminDownMsgs, AdminUpMsgs}, messages::{MessagesUpMsgs, MessagesDownMsgs},
+// };
+pub type UserId = EntityId;
+
+
+
+
+#[derive(Debug, Serialize, Deserialize, Clone)]
+#[serde(crate = "serde")]
+pub struct User {
+    pub id: UserId,
+    pub name: String,
+    pub auth_token: AuthToken,
+}
+
+
+#[derive(Debug, Serialize, Deserialize, Clone)]
+#[serde(crate = "serde")]
+pub enum UpMsg {
+    // ------ Auth ------
+    Login { email: String, password: String },
+    // ForgetPassword{email: String},
+    // ResetPassword(ResetForm),
+    // Signin { form: signin::SigninForm },
+    // Logout,
+    // AddSchool { name: String },
+    // Register(String, String),
+    // UpdateSchool(FullSchool),
+    // GetSchool,
+    // Classes(ClassUpMsgs),
+    // Teachers(TeacherUpMsgs),
+    // Lectures(LecturesUpMsg),
+    // Timetables(TimetablesUpMsgs),
+    // Messages(MessagesUpMsgs),
+    // Admin(AdminUpMsgs)
+}
+
+
+
+
+#[derive(Debug, Serialize, Deserialize)]
+#[serde(crate = "serde")]
+pub enum DownMsg {
+    // ------ Auth ------
+    LoginError(String),
+    LoggedIn(User),
+    // SigninError(String),
+    // Signin,
+    // Register,
+    // ResetPassword,
+    // LoggedOut,
+    // LoggedOutError(String),
+    // Registered(User),
+    // ResgiterErrors,
+    // GetSchool { id: i32, name: String },
+    // AddedSchool(School),
+    // AddSchoolError(String),
+    // UpdateSchool,
+    // Auth(i32),
+    // AuthError(String),
+    // Classes(ClassDownMsgs),
+    // Teachers(TeacherDownMsgs),
+    // Lectures(LecturesDownMsg),
+    // Timetables(TimetablesDownMsgs),
+    // Messages(MessagesDownMsgs),
+    // Admin(AdminDownMsgs)
+}
